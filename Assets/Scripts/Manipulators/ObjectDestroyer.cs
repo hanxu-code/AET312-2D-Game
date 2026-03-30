aab1b5454c22c02b4f7be8fe5c746acff9ee8b4d
Han Xu <hxu25085@gmail.com>
2026-03-30 03:45:34 -0500

    Five Final Challenges #2
    Finish two of the Five Final Challenges

--- Assets/Scripts/Manipulators/ObjectDestroyer.cs
+++ Assets/Scripts/Manipulators/ObjectDestroyer.cs
@@ -25,4 +25,37 @@ public class ObjectDestroyer : MonoBehaviour
     {
         PlayerController.instance.Respawn();
     }
+
+    // Allow for destroying static objects
+    public void DestroyNearbyStaticObjects()
+    {
+        // Get the circle collider on this object to know the explosion radius
+        CircleCollider2D circle = GetComponent<CircleCollider2D>();
+        if (circle == null) return;
+
+        float radius = circle.radius * transform.lossyScale.x;
+        Vector2 center = (Vector2)transform.position + circle.offset;
+
+        // Find all colliders within the explosion radius
+        Collider2D[] hits = Physics2D.OverlapCircleAll(center, radius);
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.CompareTag("StaticObject"))
+            {
+                if (consoleLog) Debug.Log("Destroying static object: " + hit.gameObject.name);
+                Destroy(hit.gameObject);
+            }
+        }
+    }
+
+    //
+    public void RespawnPlayer2(GameObject aObject)
+    {
+        PlayerController player = aObject.GetComponent<PlayerController>();
+        if (player != null)
+        {
+            player.Respawn();
+            if (consoleLog) Debug.Log("Respawning player");
+        }
+    }
 }
